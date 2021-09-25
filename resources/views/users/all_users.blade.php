2ebb22bc35452833363a31377445f3ac2a044b62
mohamedelfert <me_me4152@yahoo.com>
2021-09-25 21:09:06 +0200

    example how use laravel collective in blade file with form

--- resources/views/users/all_users.blade.php
+++ resources/views/users/all_users.blade.php
@@ -49,32 +49,57 @@
 
     <div class="content">
         <div class="col-lg-4" style="float: left">
-            <form method="post" action="{{ url('insert/user') }}">
-                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">UserName</label>
-                    <input type="text" class="form-control" name="username" value="{{old('username')}}" id="exampleFormControlInput1" placeholder="Your Name">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput2" class="form-label">Your address</label>
-                    <input type="text" class="form-control" name="address" value="{{old('address')}}" id="exampleFormControlInput2" placeholder="Your Address">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput3" class="form-label">Your Age</label>
-                    <input type="number" class="form-control" name="age" value="{{old('age')}}" id="exampleFormControlInput3" placeholder="Your Age">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput4" class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" value="{{old('email')}}" id="exampleFormControlInput4" placeholder="Your Email">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput5" class="form-label">Phone Number</label>
-                    <input type="text" class="form-control" name="phone" value="{{old('phone')}}" id="exampleFormControlInput5" placeholder="Your Phone">
-                </div>
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-primary mb-3">Add New</button>
-                </div>
-            </form>
+
+            {!! Form::open(['url'=>'insert/user']) !!}
+            <div class="mb-3">
+                {!! Form::label('exampleFormControlInput1','UserName',['class'=>'form-label']) !!}
+                {!! Form::text('username',old('username'),['class'=>'form-control','id'=>'exampleFormControlInput1','placeholder'=>'Your Name']) !!}
+            </div>
+                {!! Form::label('exampleFormControlInput2','Your Address',['class'=>'form-label']) !!}
+                {!! Form::text('address',old('address'),['class'=>'form-control','id'=>'exampleFormControlInput2','placeholder'=>'Your Address']) !!}
+            <div class="mb-3">
+                {!! Form::label('exampleFormControlInput3','Age',['class'=>'form-label']) !!}
+                {!! Form::number('age',old('age'),['class'=>'form-control','id'=>'exampleFormControlInput3','placeholder'=>'Your Age']) !!}
+            </div>
+                {!! Form::label('exampleFormControlInput4','email',['class'=>'form-label']) !!}
+                {!! Form::email('email',old('email'),['class'=>'form-control','id'=>'exampleFormControlInput4','placeholder'=>'Your Email']) !!}
+            <div class="mb-3">
+                {!! Form::label('exampleFormControlInput5','Phone',['class'=>'form-label']) !!}
+                {!! Form::text('phone',old('phone'),['class'=>'form-control','id'=>'exampleFormControlInput5','placeholder'=>'Your Phone']) !!}
+            </div>
+            <div class="mb-3">
+                {!! Form::submit('Add New',['class'=>'btn btn-primary mb-3']) !!}
+            </div>
+            {!! Form::close() !!}
+
+
+{{--            <form method="post" action="{{ url('insert/user') }}">--}}
+{{--                <input type="hidden" name="_token" value="{{csrf_token()}}">--}}
+{{--                <div class="mb-3">--}}
+{{--                    <label for="exampleFormControlInput1" class="form-label">UserName</label>--}}
+{{--                    <input type="text" class="form-control" name="username" value="{{old('username')}}" id="exampleFormControlInput1" placeholder="Your Name">--}}
+{{--                </div>--}}
+{{--                <div class="mb-3">--}}
+{{--                    <label for="exampleFormControlInput2" class="form-label">Your address</label>--}}
+{{--                    <input type="text" class="form-control" name="address" value="{{old('address')}}" id="exampleFormControlInput2" placeholder="Your Address">--}}
+{{--                </div>--}}
+{{--                <div class="mb-3">--}}
+{{--                    <label for="exampleFormControlInput3" class="form-label">Your Age</label>--}}
+{{--                    <input type="number" class="form-control" name="age" value="{{old('age')}}" id="exampleFormControlInput3" placeholder="Your Age">--}}
+{{--                </div>--}}
+{{--                <div class="mb-3">--}}
+{{--                    <label for="exampleFormControlInput4" class="form-label">Email address</label>--}}
+{{--                    <input type="email" class="form-control" name="email" value="{{old('email')}}" id="exampleFormControlInput4" placeholder="Your Email">--}}
+{{--                </div>--}}
+{{--                <div class="mb-3">--}}
+{{--                    <label for="exampleFormControlInput5" class="form-label">Phone Number</label>--}}
+{{--                    <input type="text" class="form-control" name="phone" value="{{old('phone')}}" id="exampleFormControlInput5" placeholder="Your Phone">--}}
+{{--                </div>--}}
+{{--                <div class="col-auto">--}}
+{{--                    <button type="submit" class="btn btn-primary mb-3">Add New</button>--}}
+{{--                </div>--}}
+{{--            </form>--}}
+
         </div>
         <div class="col-lg-4" style="float: right">
             @if($errors->any())
