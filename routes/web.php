8a35d038d315ec2a71f9b58077054c485f2b54b5
mohamedelfert <me_me4152@yahoo.com>
2021-09-28 20:08:01 +0200

    new

--- routes/web.php
+++ routes/web.php
@@ -26,54 +26,57 @@ Route::delete('delete/news/{id?}','NewsController@delete')->where('id','[0-9]+')
 //Route::post('test/1', function (Illuminate\Http\Request $request){
 //    return $request->all();
 //});
+//
+//
+//Route::get('user/{id?}/{name?}', function ($id = null , $name = null){
+//    return 'Welcome From User Route Your Id Is : ' . $id . ' And UserName Is ' . $name;
+//});
+//
+//
+//
+//Route::get('user/{id?}/{name?}', function ($id = null , $name = null){
+//    return 'Welcome From User Route Your Id Is : ' . $id . ' And UserName Is ' . $name;
+//})->where(['id' => '[0-9]+' , 'name'=> '[a-zA-Z]+']);
+//
+//
+//Route::get('test', function (){
+//    return '
+//        <form method="post" action="">
+//            <input type="hidden" name="_token" value="'.csrf_token().'">
+//            <input type="text" name="foo">
+//            <input type="submit" name="send">
+//            <input type="hidden" name="_method" value="delete">
+//        </form>
+//    ';
+//});
+//
+//Route::any('test', function (){
+//    return 'Welcome From any route ' . request('foo');
+//});
+//
+//Route::post('test', function (){
+//    return 'Welcome From Test ' . request('foo');
+//});
+//
+//Route::put('test', function (){
+//    return 'Welcome From PUT ' . request('foo');
+//});
+//
+//Route::patch('test', function (){
+//    return 'Welcome From PATCH ' . request('foo');
+//});
+//
+//Route::delete('test', function (){
+//    return 'Welcome From DELETE ' . request('foo');
+//});
+//
+//get
+//post
+//put
+//patch
+//delete
+//any
 
-/*
-Route::get('user/{id?}/{name?}', function ($id = null , $name = null){
-    return 'Welcome From User Route Your Id Is : ' . $id . ' And UserName Is ' . $name;
-});
-*/
+Auth::routes();
 
-/*
-Route::get('user/{id?}/{name?}', function ($id = null , $name = null){
-    return 'Welcome From User Route Your Id Is : ' . $id . ' And UserName Is ' . $name;
-})->where(['id' => '[0-9]+' , 'name'=> '[a-zA-Z]+']);
-*/
-/*
-Route::get('test', function (){
-    return '
-        <form method="post" action="">
-            <input type="hidden" name="_token" value="'.csrf_token().'">
-            <input type="text" name="foo">
-            <input type="submit" name="send">
-            <input type="hidden" name="_method" value="delete">
-        </form>
-    ';
-});
-
-Route::any('test', function (){
-    return 'Welcome From any route ' . request('foo');
-});
-/*
-Route::post('test', function (){
-    return 'Welcome From Test ' . request('foo');
-});
-
-Route::put('test', function (){
-    return 'Welcome From PUT ' . request('foo');
-});
-
-Route::patch('test', function (){
-    return 'Welcome From PATCH ' . request('foo');
-});
-
-Route::delete('test', function (){
-    return 'Welcome From DELETE ' . request('foo');
-});
-
-get
-post
-put
-patch
-delete
-any
-*/
+Route::get('/home', 'HomeController@index')->name('home');
